edfc0068f0a89e2bf35f0aad77c1314420f41a79
nikhsub <venkatasubr2@wisc.edu>
2022-09-21 10:34:24 -0400

    Added background subtraction

--- LAB1_GammaRaySpectroscopy/analysis.py
+++ LAB1_GammaRaySpectroscopy/analysis.py
@@ -1,8 +1,24 @@
 import matplotlib.pyplot as plt
 import mcareader as mca
-spec = mca.Mca("Co_10cm_1uC.mca")
-x = spec.get_points()[0]
-y = spec.get_points()[1]
-plt.yscale("log")
-plt.plot(x,y)
-plt.show()
+spec1 = mca.Mca("Co_10cm_1uC.mca")
+spec2 = mca.Mca("bkg_without_top_bricks.mca")
+# print(spec1.get_calibration_points())
+# print(spec2.get_calibration_points())
+# print(spec2.raw)
+x1 = spec1.get_points()[0]
+y1 = spec1.get_points()[1]
+x2 = spec2.get_points()[0]
+y2 = spec2.get_points()[1]
+plt.plot(x1, y1/float(spec1.get_variable("Live Time")), 'r', label = "Co Sig")
+plt.plot(x2, y2/float(spec2.get_variable("Live Time")), 'b', label = "Bkg")
+plt.plot(x1, y1/float(spec1.get_variable("Live Time"))-y2/float(spec2.get_variable("Live Time")), 'g', label = "Sig-Bkg")
+plt.legend()
+plt.ylim([0,0.10])
+plt.xlim([200, 2400])
+plt.xlabel("Energy (keV)")
+plt.ylabel("Counts/sec")
+plt.savefig("present.png")
+
+
+#Subtract after cutting out unimportant bins or before?
+#Does the graph make sense
